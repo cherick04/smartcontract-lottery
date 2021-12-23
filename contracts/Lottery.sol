4e1183c2bd648ee66a73e43c16d0bd727d8f8910
Erick C <cherick04@hotmail.com>
2021-12-23 14:23:47 -0300

    Adding Chainlink VRF to generate random numbers

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,19 +3,44 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
-    constructor(address _priceFeedAddress) public {
+    // From RandomNumberConsumer contract
+    uint256 public fee;
+    bytes32 public keyHash;
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyHash = _keyhash;
     }
 
     function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.CLOSED);
         // $50 minimum
+        require(msg.value > getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
@@ -30,7 +55,45 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {
+        /* // Pseudo random [Not secure]
+        uint256(
+            keccak256( // this hashing algorith always hashes the same way
+                abi.encodePack(
+                    nonce, // (transaction number) is predictable
+                    msg.sender, // predictable
+                    block.difficulty, // can actually be manipulated by miners
+                    block.timestamp // predictable
+                )
+            )
+        ) % players.length; */
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+        require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
 }
