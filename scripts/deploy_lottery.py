92c0a8f3466dc0c5666591ebf56036d9eb1400f9
Erick C <cherick04@hotmail.com>
2021-12-23 16:31:33 -0300

    Added deploy files

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,29 @@
+from brownie import Lottery, network, config
+from scripts.helpful_scripts import get_account, get_contract
+
+
+def deploy_lottery():
+    account = get_account()
+    Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Lottery deployed!")
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("Lottery started!")
+
+
+def main():
+    deploy_lottery()
+    start_lottery()
